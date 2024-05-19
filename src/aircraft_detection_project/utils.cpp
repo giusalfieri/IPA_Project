5fc2a186a5da1d6fc151f60460d3a53a127383cb
Giuseppe Alfieri <150775639+giusalfieri@users.noreply.github.com>
2024-05-19 11:50:13 +0200

    Update utils.cpp

--- src/aircraft_detection_project/utils.cpp
+++ src/aircraft_detection_project/utils.cpp
@@ -1,10 +1,11 @@
 #include "utils.h"
 
+
+
 double degrees2rad(double degrees)
 {
 	return (degrees / 180) * ipa::PI;
 }
-
 
 
 double rad2degrees(double radians)
@@ -12,16 +13,34 @@ double rad2degrees(double radians)
 	return (radians / ipa::PI) * 180;
 }
 
+bool sortByDescendingArea(object& first, object& second)
+{
+	return cv::contourArea(first) > contourArea(second);
+}
+
+cv::Rect Yolo2BRect(const cv::Mat& img, double x_center, double y_center, double width, double height)
+{
+	// Convert normalized coordinates to pixel values
+	int x_center_px = ucas::round<float>(x_center * img.cols);
+	int y_center_px = ucas::round<float>(y_center * img.rows);
+	int width_px = ucas::round<float>(width * img.cols);
+	int height_px = ucas::round<float>(height * img.rows);
+
+	// Calculate top left corner of bounding box
+	int x = x_center_px - width_px / 2;
+	int y = y_center_px - height_px / 2;
+
+	return cv::Rect(x, y, width_px, height_px);
+}
 
 
 cv::Mat getRotationROI(cv::Mat& img, cv::Rect& roi) {
+
 	cv::Mat rotation_roi;
 
 	// Check if the ROI is within the image boundaries
 	if (roi.x >= 0 && roi.y >= 0 && roi.x + roi.width <= img.cols && roi.y + roi.height <= img.rows)
-	{
 		rotation_roi = img(roi);
-	}
 	else
 	{
 		// If the ROI is out of the image boundaries, pad the image
@@ -42,32 +61,6 @@ cv::Mat getRotationROI(cv::Mat& img, cv::Rect& roi) {
 
 
 	return rotation_roi;
-}
-
-
-
-cv::Rect Yolo2BRect(const cv::Mat& input_img, double x_center, double y_center, double width, double height)
-{
-	int imageWidth = input_img.cols;
-	int imageHeight = input_img.rows;
-
-	// Convert normalized coordinates to pixel values
-	int x_center_px = ucas::round<float>(x_center * imageWidth);
-	int y_center_px = ucas::round<float>(y_center * imageHeight);
-	int width_px = ucas::round<float>(width * imageWidth);
-	int height_px = ucas::round<float>(height * imageHeight);
-
-	// Calculate top left corner of bounding box
-	int x = x_center_px - width_px / 2;
-	int y = y_center_px - height_px / 2;
-
-	return cv::Rect(x, y, width_px, height_px);
-}
-
-
-bool sortByDescendingArea(object& first, object& second)
-{
-	return cv::contourArea(first) > contourArea(second);
 }
 
 
@@ -106,4 +99,21 @@ cv::Mat rotate90(cv::Mat img, int step)
 	}
 
 	return img_rot;
+}
+
+
+std::filesystem::path createDirectoryInParent(const std::string& folder_path, const std::string& directory_name)
+{
+	// Ottieni il percorso della directory padre
+	std::filesystem::path dataset_path(folder_path);
+	std::filesystem::path parent_path = dataset_path.parent_path();
+
+	// Crea un nuovo percorso per la nuova directory
+	std::filesystem::path new_dir_path = parent_path / directory_name;
+
+	if (!std::filesystem::exists(new_dir_path))
+		std::filesystem::create_directory(new_dir_path);
+
+
+	return new_dir_path;
 }
