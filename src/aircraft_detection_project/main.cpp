2bfe2af5dc54d06020112f96d1ff46b840943c57
Giuseppe Alfieri <150775639+giusalfieri@users.noreply.github.com>
2024-05-21 17:05:57 +0200

    Update main.cpp

--- src/aircraft_detection_project/main.cpp
+++ src/aircraft_detection_project/main.cpp
@@ -1,11 +1,17 @@
 #include "template_extraction.h"
 
 
+//#define USE_EXTRACT_TEMPLATES
+
 
 int main()
 {
-
-
+	
+	//#ifdef USE_EXTRACT_TEMPLATES
+		extractTemplates();
+	//#endif
+		
+	
 
 	return EXIT_SUCCESS;
 }
