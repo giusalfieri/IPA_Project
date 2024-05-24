497547ce0ac8a5f9d077f77aa0ac33426ee140e3
ricdag8 <139982812+ricdag8@users.noreply.github.com>
2024-05-24 23:32:51 +0200

    Update eigenPlanes.cpp

--- src/aircraft_detection_project/eigenPlanes.cpp
+++ src/aircraft_detection_project/eigenPlanes.cpp
@@ -1,1 +1,42 @@
 #include "eigenPlanes.h"
+#include "eigenPlanes.h"
+#include "utils.h"
+
+cv::Mat createDataMatrix(const std::vector<cv::Mat>& images);
+
+cv::Mat eigenPlanes(const std::vector<cv::Mat>& vec, const int img_dim)
+{
+    //Centrare tutti i dati rispetto la media
+    //Ridimensionamento delle immagini, stesse dimensioni
+    //Caricamento immagini in una singola matrice
+    //Calcolo della matrice di covarianza
+    //Calcolo degli autovettori e degli autovalori
+
+    cv::Mat data = createDataMatrix(vec);
+
+    cv::PCA pca(data, cv::Mat(), cv::PCA::DATA_AS_ROW);
+
+    cv::Mat avg_airplane = pca.mean.reshape(1, img_dim);
+
+    return avg_airplane;
+
+}
+
+
+
+cv::Mat createDataMatrix(const std::vector<cv::Mat>& images)
+{
+	cv::Mat data;
+	for (const cv::Mat& img : images)
+	{
+        cv::Mat imgVector = img.reshape(1, 1);
+		data.push_back(imgVector);
+	}
+
+	return data;
+}
+
+
+
+
+
