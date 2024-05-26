bde07a7ad056baede7bb2ab84d0b7d95ba9a3e75
Giuseppe Alfieri <150775639+giusalfieri@users.noreply.github.com>
2024-05-26 12:02:07 +0200

    Update eigenPlanes.cpp

--- src/aircraft_detection_project/eigenPlanes.cpp
+++ src/aircraft_detection_project/eigenPlanes.cpp
@@ -3,18 +3,18 @@
 
 cv::Mat createDataMatrix(const std::vector<cv::Mat>& images);
 
+
 cv::Mat eigenPlanes(const std::vector<cv::Mat>& vec, const int img_dim)
 {
-	
-    cv::Mat data = createDataMatrix(vec);
+	cv::Mat data = createDataMatrix(vec);
 
-    cv::PCA pca(data, cv::Mat(), cv::PCA::DATA_AS_ROW);
+	cv::PCA pca(data, cv::Mat(), cv::PCA::DATA_AS_ROW, 0.95);
 
-    cv::Mat avg_airplane = pca.mean.reshape(1, img_dim);
+	cv::Mat avg_airplane = pca.mean.reshape(1, img_dim);
 
-    return avg_airplane;
+
+	return avg_airplane;
 }
-
 
 
 cv::Mat createDataMatrix(const std::vector<cv::Mat>& images)
@@ -22,7 +22,7 @@ cv::Mat createDataMatrix(const std::vector<cv::Mat>& images)
 	cv::Mat data;
 	for (const cv::Mat& img : images)
 	{
-        cv::Mat imgVector = img.reshape(1, 1);
+		cv::Mat imgVector = img.reshape(1, 1);
 		data.push_back(imgVector);
 	}
 
