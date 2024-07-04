21e4646d5ff65fa6bca06c521b59a2c43b41e888
Giuseppe Alfieri <150775639+giusalfieri@users.noreply.github.com>
2024-07-04 17:07:41 +0200

    Delete src/aircraft_detection_project directory

--- src/aircraft_detection_project/eigenplanes.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "ipaConfig.h"
-#include "ucasConfig.h"
-
-
-
-cv::Mat eigenPlanes(const std::vector<cv::Mat>& vec, cv::Size img_dims);
--- src/aircraft_detection_project/template_matching.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <opencv2/opencv.hpp>
-#include <vector>
-
-
-std::vector<cv::Point> templateMatching(const cv::Mat& src_img);
