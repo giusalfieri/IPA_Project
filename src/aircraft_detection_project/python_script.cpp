b035f5be6b40c42a943f7c3153e0ef88afa00ae5
Paolo Simeone <64153792+bonoboprog@users.noreply.github.com>
2024-07-05 19:52:22 +0200

    Add files via upload

--- src/aircraft_detection_project/python_script.cpp
+++ src/aircraft_detection_project/python_script.cpp
@@ -8,6 +8,7 @@ void configureAndRunPythonScript()
     // Path to the virtual environment
     const std::string venvPath = VENV_PATH;
 
+
     // Check if the virtual environment exists
     if (!std::filesystem::exists(venvPath))
     {
@@ -32,6 +33,35 @@ void configureAndRunPythonScript()
     const auto pythonScript = R"(
 import matplotlib.pyplot as plt
 import numpy as np
+import csv
+import os
+from pathlib import Path
+import sys
+
+sco_file = Path(r'D:\IPA\Sources\IPA_Project-main\src\false_positive.txt')
+csv_file = Path(r'D:\IPA\Sources\IPA_Project-main\src\roi_label_pairs.csv')
+output_file = Path(r'D:\IPA\Sources\IPA_Project-main\src\output.csv')
+
+# Read data from .sco file
+with open(sco_file, 'r') as f:
+    lines = f.readlines()
+
+# Remove header and take the second column
+sco_data = [line.split()[1] for line in lines[1:]]
+
+# Read data from .csv file
+with open(csv_file, 'r') as f:
+    reader = csv.reader(f)
+    csv_data = [row[4] for row in reader]
+
+# Write data to new CSV file in two columns
+with open(output_file, 'w', newline='') as f:
+    writer = csv.writer(f)
+    # Use zip to pair sco_data and csv_data
+    for csv_val, sco in zip(csv_data, sco_data):
+        writer.writerow([csv_val, sco])
+
+
 
 # Generate x values
 x = np.linspace(0, 2 * np.pi, 100)
