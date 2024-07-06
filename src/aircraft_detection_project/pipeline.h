685ca04af9d43dd29d34ecabf59dd95fd2c0ec77
Giuseppe Alfieri <150775639+giusalfieri@users.noreply.github.com>
2024-07-06 16:33:25 +0200

    Add files via upload

---
+++ src/aircraft_detection_project/pipeline.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+
+
+void extract_csv_for_svm_training();
+
+void extractTemplates();
+
+void performKMeansBySize();
+
+void performKMeansByIntensity();
+
+void resizeImagesAcrossClusters();
+
+void generateEigenplanes();
+
+void analyzeImg(const std::string& testing_img_id);
+
+void evaluatePerformance();
+
+void printHelp();
+
+void parseArguments(int argc, char** argv, std::vector<std::string>& steps);
+
+void checkPreviousStep(const std::string& current_step);
+
+void executeStep(const std::string& step);
