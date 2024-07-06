113df5cc405f2a35dd61fd7dcec0b83078ce3712
Giuseppe Alfieri <150775639+giusalfieri@users.noreply.github.com>
2024-07-06 16:32:59 +0200

    Delete src/aircraft_detection_project directory

--- src/aircraft_detection_project/hog_features_extraction.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <opencv2/opencv.hpp>
-#include <string>
-#include <vector>
-
-
-std::vector<std::vector<float>> hog_features_extraction(const std::vector<cv::Rect>& rois, const cv::Mat& image);
-
-void writeHogFeaturesToCsv(const std::vector<std::vector<float>>& hog_features, const std::string& filename);
