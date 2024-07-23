4783906366bd1f0b700fc07f1850d1b3d6af88db
Giuseppe Alfieri <150775639+giusalfieri@users.noreply.github.com>
2024-07-23 22:24:38 +0200

    Add files via upload

---
+++ doc/search/files_5.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['straight_5fairplanes_5fextraction_2ecpp_0',['straight_airplanes_extraction.cpp',['../d7/dcf/straight__airplanes__extraction_8cpp.html',1,'']]],
+  ['straight_5fairplanes_5fextraction_2eh_1',['straight_airplanes_extraction.h',['../da/d36/straight__airplanes__extraction_8h.html',1,'']]],
+  ['svm_5ftraining_2ecpp_2',['svm_training.cpp',['../d5/dc2/svm__training_8cpp.html',1,'']]],
+  ['svm_5ftraining_2eh_3',['svm_training.h',['../d1/d31/svm__training_8h.html',1,'']]]
+];
---
+++ doc/search/files_6.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['template_5fmatching_2ecpp_0',['template_matching.cpp',['../dd/d1a/template__matching_8cpp.html',1,'']]],
+  ['template_5fmatching_2eh_1',['template_matching.h',['../dd/d64/template__matching_8h.html',1,'']]]
+];
