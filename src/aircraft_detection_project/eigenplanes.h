a61b1c587d96f507990ce4509fafc7fbacfab93e
Giuseppe Alfieri <150775639+giusalfieri@users.noreply.github.com>
2024-07-23 15:33:09 +0200

    Delete src/aircraft_detection_project directory

--- src/aircraft_detection_project/eigenplanes.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <opencv2/core/mat.hpp>
-
-
-cv::Mat eigenPlanes(const std::vector<cv::Mat>& vec, cv::Size img_dims);
--- src/aircraft_detection_project/python_script.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <Python.h>
-
-
-void configureAndRunPythonScript();
